4b5757729fe9514b4d551d3402427397107ee626
Firaol Admasu <firaolfro@gmail.com>
2025-04-21 13:30:58 +0300

    updated  app/verifyEmail/page.js

--- app/verifyEmail/page.js
+++ app/verifyEmail/page.js
@@ -1,4 +1,12 @@
-import EmailVerificationWrapper from "./EmailVerificationWrapper";
+// app/verifyEmail/page.tsx or page.jsx
+import dynamic from "next/dynamic";
+
+const EmailVerificationWrapper = dynamic(
+  () => import("./EmailVerificationWrapper"),
+  {
+    ssr: false, // Prevents Next.js from rendering it on the server
+  }
+);
 
 export default function VerifyEmailPage() {
   return <EmailVerificationWrapper />;
