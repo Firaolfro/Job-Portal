f714a7236d1917394cbc78f6b23fcda7b30c6540
Firaol Admasu <firaolfro@gmail.com>
2025-05-22 07:50:38 +0300

    updated employer dashboard

--- app/browse-candidate/layout.js
+++
@@ -1,5 +0,0 @@
-import BaseLayout from "@/components/layouts/BaseLayout";
-
-export default function EmployerLayout({ children }) {
-  return <BaseLayout menuKey="employer">{children}</BaseLayout>;
-}
--- app/candidate-dashboard/settings/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className="font-bold pt-5">
-      setting page
-      </div>
-  )
-}
